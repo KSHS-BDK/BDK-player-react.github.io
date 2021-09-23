1a044b309fb16214dece7a9d3140145ef2349c6c
Yuwei Wang <yuwei.wang.acbel@gmail.com>
2021-09-23 13:00:45 +0800

    Change musiclist.json path.

--- src/store/actions/music.js
+++ src/store/actions/music.js
@@ -2,7 +2,7 @@ import { SET_MUSIC_LIST } from '../actionTypes';
 
 import { initAllCategories } from './category';
 
-import musiclist from '../../utils/data/musiclist.json';
+import musiclist from '../../data/musiclist.json';
 
 export const setMusicList = () => {
     return {
--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ export function configureStore() {
         rootReducer,
         compose(
             applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     );
     return store
